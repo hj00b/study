c7443c559a19c07f44d644c9a086ea5fabef2022
hyejeong <hjgit00b@gmail.com>
2025-02-04 21:30:40 +0900

    백준 A + B - C 문제 풀이

---
+++ BAEKJOON/bj_31403.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = input()
+c = int(input())
+b_len = len(b) 
+b = int(b)
+print(a+b-c)
+
+calc = a*(10**(b_len)) + b - c
+print(calc)
+
+
+# 스터디원 답안
+# print(int(a)+int(b)-int(c))
+# print(int(a+b)-int(c))
+
+# 내 코드 리뷰
+# 문제를 단순하게 바라보기, 코드 가독성 고려하기
