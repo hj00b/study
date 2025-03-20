14205a5217681ac249309c3e9bcd3a03c0e764c6
hj <hjgit00b@gmail.com>
2025-03-20 17:23:43 +0900

    [D4] Title: 최소 스패닝 트리, Time: 8,683 ms, Memory: 115,856 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.py
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,36 +1,41 @@
-from heapq import heappop, heappush
+def find_set(x):
+    if p[x] == x:
+        return x
+    p[x] = find_set(p[x])
+    return p[x]
 
 
-def prim(start_node):
-    # 우선순위 큐를 사용해서 현재 노드에서 가장 가중치가 작은 것 선택
-    pq = [(0, start_node)]
-    MST = [0] * (V+1)
+def union(x, y):
+    x_boss = find_set(x)
+    y_boss = find_set(y)
 
-    min_w = 0
+    if x_boss == y_boss:
+        return
 
-    while pq:
-        node_w, node = heappop(pq)
-        if MST[node]:
-            continue
-        MST[node] = 1
-        min_w += node_w
-
-        # 이동한 자리에서 방문 가능한 노드 찾기
-        for next_node in graph[node]:
-            if MST[next_node[1]]:
-                continue
-            heappush(pq, next_node)
-    return min_w
+    if x_boss > y_boss:
+        p[y_boss] = x_boss
+    else:
+        p[x_boss] = y_boss
 
 
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     V, E = map(int, input().split())
-    graph = [[] for _ in range(V+1)]
-
+    edges = []
+    p = list(range(V + 1))
     for _ in range(E):
         s, e, w = map(int, input().split())
-        graph[s].append((w, e))
-        graph[e].append((w, s))
-    ans = prim(1)
-    print(f"#{tc} {ans}")
+        edges.append((w, s, e))
+
+    cnt = 0
+    result = 0
+    edges.sort(key=lambda x:x[0])
+    for w, u, v in edges:
+        if find_set(u) == find_set(v):
+            continue
+        union(u, v)
+        result += w
+        cnt += 1
+        if cnt == V - 1:
+            break
+    print(f"#{tc} {result}")
